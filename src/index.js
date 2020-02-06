4f7286b4b770cd05de89c15f0273645651306fd4
Lenocka <lvutina.en@gmail.com>
2020-02-06 22:56:33 +0300

    init: webpack setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+'use strict';
+//import React from "react";
+//import ReactDOM from "react-dom";
+//import {hot} from "react-hot-loader";
+import './styles.scss';
+
+console.log ("hello Lena world");
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+              'file-loader',
+            ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    //new ExtractTextPlugin('styles.css'),
+    
+      
+    //new HtmlWebpackPlugin({ title: 'Output Management' })
+  ],
+}
+
+module.exports = config;
